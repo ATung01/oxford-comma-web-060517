6f8b661e0b85a80385feb76e6241364055fcecb8
Alex Tung <acc.tung@gmail.com>
2017-05-29 02:32:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  result = ""
+  if array.size == 1
+    result = array.join
+  elsif array.size == 2
+    result = array.join(" and ")
+  elsif array.size > 2
+    last_thing = array.pop
+    result = array.join(", ")
+    result += ", and #{last_thing}"
+  end
+  result
 
 end
